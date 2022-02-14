a995daf429f6769d6589c40a83fb98c611236bb8
Advaith18 <tadvaith18@gmail.com.com>
2022-02-14 11:50:47 +0530

    media

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
             bot.style.color = "white";
             bot.style.backgroundColor = "black";
 
+            console.log("Loading Light Theme From Dark Theme.");
             body.style.color = "black";
             body.style.backgroundColor = "white";
         }
@@ -16,6 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
             bot.style.color = "black";
             bot.style.backgroundColor = "white";
 
+            console.log("Loading Dark Theme From Light THeme.");
             body.style.color = "white";
             body.style.backgroundColor = "black";
         }
